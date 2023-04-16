bf6d7f90674789157e491e7d32be7d56b77a8089
kimpossibleee <yeayoungk@gmail.com>
2023-04-16 10:02:12 -0700

    Make final changes to main.py, include main_old_explicit.py

--- floor.py
+++
@@ -1,6 +0,0 @@
-class Floor:
-    def __init__(self):
-        self.handicap = 2
-        self.compact = 10
-        self.regular = 18
-
--- main_old_explicit.py
+++ main_old_explicit.py
@@ -14,14 +14,14 @@ class ParkingGarage:
         this_plate = input("Please enter your plate number: ")
         if this_plate in self.all_tickets["active"]:
             print("This car already has an active ticket")
-        else:
-            self.add_ticket(tk.Ticket(this_plate, start_time = dt.datetime.now()))
+            self.main()
+        self.add_ticket(tk.Ticket(this_plate, start_time = dt.datetime.now()))
 
     def add_ticket(self, this_ticket):
         self.all_tickets["active"][this_ticket.plate_num] = this_ticket
         self.spaces_remaining -= 1
         this_ticket.print_ticket()
-
+        self.main()
 
     def leave_garage(self):
         this_plate = input("Please enter plate number for car leaving: ")
@@ -34,16 +34,19 @@ class ParkingGarage:
         self.sales += float(self.all_tickets["active"][this_plate].total)
         self.all_tickets["paid"][this_plate] = self.all_tickets["active"][this_plate]
         del(self.all_tickets["active"][this_plate])
+        self.main()
 
     def full_lot_check(self):
         if self.spaces_remaining < 1:
             print("This lot is full, you'll have to park elsewhere.")
+            self.main()
 
     def leave_check(self):
         if self.all_tickets["active"]:
             self.leave_garage()
         else:
             print("No cars present.")
+            self.main()
 
     def garage_status(self):
         print(f'Number of spots remaining: {self.spaces_remaining}\n')
@@ -81,24 +84,27 @@ class ParkingGarage:
         else:
             _ = system('clear')
 
+
     def main(self):
-        while True:
-            next_action = input("\nDoes the next car wish to 'enter' or 'exit' the parking garage?\n'Status' to view garage spots and tickets info,\n'history' to view paid tickets, or 'quit' to stop managing garage: ").lower()
-            self.clear()
-            if "enter" in next_action:
-                self.park()
-            elif "exit" in next_action:
-                self.leave_check()
-            elif "status" in next_action:
-                self.garage_status()
-            elif "history" in next_action:
-                self.ticket_history()
-            elif "quit" in next_action:
-                print("Thanks for managing the garage.\n")
-                self.master_log()
-                return self.all_tickets
-            else:
-                print("Please enter a vaild key")
+        next_action = input("\nDoes the next car wish to 'enter' or 'exit' the parking garage?\n'Status' to view garage spots and tickets info,\n'history' to view paid tickets, or 'quit' to stop managing garage: ").lower()
+        self.clear()
+        if "enter" in next_action:
+            self.park()
+        elif "exit" in next_action:
+            self.leave_check()
+        elif "status" in next_action:
+            self.garage_status()
+            self.main()
+        elif "history" in next_action:
+            self.ticket_history()
+            self.main()
+        elif "quit" in next_action:
+            print("Thanks for managing the garage.\n")
+            self.master_log()
+            return self.all_tickets
+        else:
+            print("Please enter a vaild key")
+            self.main()
 
 startThings = ParkingGarage()
 startThings.main()
